4faa7de8da6c1f000786b8ddb407877adc1e4b5a
Mohit Gupta <Mohit.Gupta14@ibm.com>
2024-07-18 12:24:57 +0530

    Initial Commit

---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Mohit Gupta on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
