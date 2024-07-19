04ffb0f57c6b0fe86a2d9f9e5d3890170cd3144d
Mohit Gupta <Mohit.Gupta14@ibm.com>
2024-07-19 18:36:21 +0530

    Scroll View

--- VacationInVegas/ScrollImage.swift
+++ VacationInVegas/ScrollImage.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 
 struct ScrollImage: View {
+    let image : String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(image)
+            .resizable()
+            .scaledToFit()
+            .clipShape(.rect(cornerRadius: 20))
+            .scrollTransition { content, phase in
+                content.scaleEffect(phase.isIdentity ? 1 : 0.5)
+                    .opacity(phase.isIdentity ? 1 : 0.5)
+            }
     }
 }
 
 #Preview {
-    ContentView()
+    ScrollImage(image: "bellagio")
 }
