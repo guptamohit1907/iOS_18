4822f14b1b1aae7e1d505a2a634ac6cac5076e01
Mohit Gupta <Mohit.Gupta14@ibm.com>
2024-07-19 16:58:01 +0530

    My First Commit

---
+++ VacationInVegas/ScrollImage.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VacationInVegas
+//
+//  Created by Mohit Gupta on 18/07/24.
+//
+
+import SwiftUI
+
+struct ScrollImage: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VacationInVegas/Symbols.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  VacationInVegas
+//
+//  Created by Mohit Gupta on 18/07/24.
+//
+
+import SwiftUI
+
+struct Symbols: View {
+    @State private var shouldBounce = false
+    @State private var shouldRotate = false
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .symbolEffect(.pulse)
+            
+            Image(systemName: "airplane")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.teal)
+                .symbolEffect(.wiggle)
+            
+            Image(systemName: "wifi")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.purple)
+                .symbolEffect(.variableColor.reversing)
+            
+            Image(systemName: "person.crop.circle.badge.plus")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .symbolRenderingMode(.multicolor)
+                .symbolEffect(.bounce,value: shouldBounce)
+                .onTapGesture {
+                    shouldBounce.toggle()
+                }
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.gray, .yellow, .mint)
+                .symbolEffect(.bounce,value: shouldBounce)
+                .onTapGesture {
+                    shouldBounce.toggle()
+                }
+            
+            Image(systemName: "arrow.counterclockwise")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.blue.mix(with: .red, by: 0.25))
+                .symbolEffect(.rotate,value: shouldRotate)
+                .onTapGesture {
+                    shouldRotate.toggle()
+                }
+            
+            Image(systemName: "sun.max.fill")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.yellow)
+                .symbolEffect(.breathe)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    Symbols()
+}
