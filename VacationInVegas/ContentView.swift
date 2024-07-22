d263794ca6e2966345b6e3e60d3fa15c20d696d1
Mohit Gupta <Mohit.Gupta14@ibm.com>
2024-07-23 00:22:32 +0530

    Final Commit

--- VacationInVegas/ContentView.swift
+++ VacationInVegas/ContentView.swift
@@ -6,19 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView {
+            Tab("Places", systemImage: "photo"){
+                PlaceList()
+            }
+            
+            Tab("Trip History", systemImage: "chart.line.uptrend.xyaxis"){
+                TripsChart()
+            }
+        }.preferredColorScheme(.dark)
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(Place.preview)
 }
